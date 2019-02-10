9021ab5982393ba12a80ea1a66bb060588b56541
Lars Wobus <lars.wobus@web.de>
2019-02-10 13:13:52 +0100

    Bind reserved functions if they exist

--- packages/scene-preparation/main.js
+++ packages/scene-preparation/main.js
@@ -2,13 +2,34 @@ const { pc } = window;
 const { app } = pc.script;
 
 if (app) {
+  const bind = (array, fn, scope) => {
+    if (fn) {
+      // note: when using window.pc.ComponentSystem.bind instead, the function below would be called at last in the scene hierarchy
+      array.unshift({ f: fn, s: scope });
+    }
+  };
+
   const createEntityIncludingScripts = (config) => {
     const entity = new window.pc.Entity();
     entity.name = config.name || 'Preliminary';
     entity.addComponent('script');
     const array = Object.entries(config.scripts || []);
     array.forEach((entry) => {
-      entity.script.create(entry[0], { attributes: entry[1] });
+      const scriptName = entry[0];
+      entity.script.create(scriptName, { attributes: entry[1] });
+      const script = entity.script[scriptName];
+      /* if (script.initialize) {
+        pc.ComponentSystem._init.unshift({
+          f: script.initialize,
+          s: script,
+        });
+      } */
+      bind(pc.ComponentSystem._init, script.initialize, script);
+      bind(pc.ComponentSystem._postInit, script.postInitialize, script);
+      bind(pc.ComponentSystem._update, script.update, script);
+      bind(pc.ComponentSystem._postUpdate, script.postUpdate, script);
+      bind(pc.ComponentSystem._fixedUpdate, script.fixedUpdate, script);
+      bind(pc.ComponentSystem._toolsUpdate, script.toolsUpdate, script);
     });
     return entity;
   };
@@ -25,11 +46,13 @@ if (app) {
       const entity = createEntityIncludingScripts(element);
       persistent.push(entity);
     });
-    // note: when using window.pc.ComponentSystem.bind instead, the function below would be called at last in the scene hierarchy
-    pc.ComponentSystem._init.unshift({
-      f: foozle.persistent[0].script.CollisionGroup.initialize,
-      s: foozle.persistent[0].script.CollisionGroup,
-    });
+
     onLibrariesLoaded();
   };
+
+  /* const start = app.start.bind(app);
+  app.start = () => {
+    start();
+    pc.app.root.addChild(window.foozle.persistent[0]);
+  }; */
 }

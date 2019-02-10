9d7add1afaa2eaa97000a451e6f0183b665df8f0
Lars Wobus <lars.wobus@web.de>
2019-02-10 11:02:34 +0100

    Initialize of test script is called at first

--- packages/scene-preparation/main.js
+++ packages/scene-preparation/main.js
@@ -1,27 +1,33 @@
-const createEntityIncludingScripts = (entityName, scriptNames) => {
+const createEntityIncludingScripts = (config) => {
   const entity = new window.pc.Entity();
-  entity.name = entityName || 'Preliminary';
+  entity.name = config.name || 'Preliminary';
   entity.addComponent('script');
-  const array = scriptNames || [];
+  const array = config.script || [];
   array.forEach((scriptName) => {
-    entity.script.create(scriptName, { attributes: {} });
+    entity.script.create(scriptName, { attributes: { _entries: [] } });
   });
   return entity;
 };
 
 let { foozle } = window;
-if (!foozle) foozle = { sceneConfig: {} };
+if (!foozle) foozle = { sceneConfig: [] };
 if (!foozle.persistent) foozle.persistent = [];
 
 const { sceneConfig, persistent } = foozle;
 
-Object.entries(sceneConfig || {}).forEach((entry) => {
-  const entity = createEntityIncludingScripts(entry[0], entry[1]);
-  persistent.push(entity);
-  // console.log(entity);
-  console.log(persistent);
-});
+const { pc } = window;
+const { app } = pc.script;
 
-// calling window.pc.app.scripts.get('CollisionGroup') does not work
-// window.pc.app is not ready at this moment
-// but window.pc.script.app.scripts.get('CollisionGroup') is already working
+const onLibrariesLoaded = app.onLibrariesLoaded.bind(app);
+app.onLibrariesLoaded = () => {
+  (sceneConfig || []).forEach((element) => {
+    const entity = createEntityIncludingScripts(element);
+    persistent.push(entity);
+  });
+  // note: when using window.pc.ComponentSystem.bind instead, the function below would be called at last in the scene hierarchy
+  pc.ComponentSystem._init.unshift({
+    f: foozle.persistent[0].script.CollisionGroup.initialize,
+    s: foozle.persistent[0].script.CollisionGroup,
+  });
+  onLibrariesLoaded();
+};

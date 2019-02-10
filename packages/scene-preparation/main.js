31ae83abe57f8c821f946fc3f6237dc6deceb611
Lars Wobus <lars.wobus@web.de>
2019-02-10 12:27:41 +0100

    Skip scene configuration in PlayCanvas editor

--- packages/scene-preparation/main.js
+++ packages/scene-preparation/main.js
@@ -1,33 +1,35 @@
-const createEntityIncludingScripts = (config) => {
-  const entity = new window.pc.Entity();
-  entity.name = config.name || 'Preliminary';
-  entity.addComponent('script');
-  const array = config.script || [];
-  array.forEach((scriptName) => {
-    entity.script.create(scriptName, { attributes: { _entries: [] } });
-  });
-  return entity;
-};
-
-let { foozle } = window;
-if (!foozle) foozle = { sceneConfig: [] };
-if (!foozle.persistent) foozle.persistent = [];
-
-const { sceneConfig, persistent } = foozle;
-
 const { pc } = window;
 const { app } = pc.script;
 
-const onLibrariesLoaded = app.onLibrariesLoaded.bind(app);
-app.onLibrariesLoaded = () => {
-  (sceneConfig || []).forEach((element) => {
-    const entity = createEntityIncludingScripts(element);
-    persistent.push(entity);
-  });
-  // note: when using window.pc.ComponentSystem.bind instead, the function below would be called at last in the scene hierarchy
-  pc.ComponentSystem._init.unshift({
-    f: foozle.persistent[0].script.CollisionGroup.initialize,
-    s: foozle.persistent[0].script.CollisionGroup,
-  });
-  onLibrariesLoaded();
-};
+if (app) {
+  const createEntityIncludingScripts = (config) => {
+    const entity = new window.pc.Entity();
+    entity.name = config.name || 'Preliminary';
+    entity.addComponent('script');
+    const array = config.script || [];
+    array.forEach((scriptName) => {
+      entity.script.create(scriptName, { attributes: { _entries: [] } });
+    });
+    return entity;
+  };
+
+  let { foozle } = window;
+  if (!foozle) foozle = { sceneConfig: [] };
+  if (!foozle.persistent) foozle.persistent = [];
+
+  const { sceneConfig, persistent } = foozle;
+
+  const onLibrariesLoaded = app.onLibrariesLoaded.bind(app);
+  app.onLibrariesLoaded = () => {
+    (sceneConfig || []).forEach((element) => {
+      const entity = createEntityIncludingScripts(element);
+      persistent.push(entity);
+    });
+    // note: when using window.pc.ComponentSystem.bind instead, the function below would be called at last in the scene hierarchy
+    pc.ComponentSystem._init.unshift({
+      f: foozle.persistent[0].script.CollisionGroup.initialize,
+      s: foozle.persistent[0].script.CollisionGroup,
+    });
+    onLibrariesLoaded();
+  };
+}

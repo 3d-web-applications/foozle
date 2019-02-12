8689e812c6ab8405a153c0daece46e2a6ffa191e
Lars Wobus <lars.wobus@web.de>
2019-02-12 23:00:48 +0100

    Trying to import funcs fail right now

---
+++ packages/--integration-test/index.mjs
@@ -0,0 +1,14 @@
+// import '@foozle/application-states';
+// import '@foozle/collision-layers';
+import createRegistry from '@foozle/function-registry';
+import '@foozle/orbit-camera';
+import '@foozle/scene-preparation';
+import '@foozle/time';
+
+describe(createRegistry, () => {
+  test('createRegistry is function', () => {
+    const expected = 'function';
+    const actual = typeof createRegistry;
+    expect(actual).toBe(expected);
+  });
+});

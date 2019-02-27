cf2504505fdf887cbc5ad720a6890f240f0febf0
Lars Wobus <lars.wobus@web.de>
2019-02-27 22:31:48 +0100

    Improving readability

--- packages/gamepad-system/src/gamepads/xbox360-input.js
+++ packages/gamepad-system/src/gamepads/xbox360-input.js
@@ -22,105 +22,97 @@ Object.defineProperty(prototype, 'gamepad', {
 prototype.initialize = function () {
   this.super();
   this.enabled = false;
-
-  this.test = false;
-};
-
-prototype.update = function () {
-  // console.log(this.gamepad.pad.buttons[0].pressed);
-  if (this.test !== this.gamepad.pad.buttons[0].pressed) {
-    this.test = this.gamepad.pad.buttons[0].pressed;
-    // console.log(this.test);
-  }
 };
 
 prototype._onGamepadChanged = function () {
   this.enabled = !!this.gamepad;
   if (this.enabled) {
+    this._pad = this.gamepad.pad;
+    this._buttons = this.gamepad.pad.buttons;
     // TODO
   }
 };
 
 prototype.aButtonPressed = function () {
-  return this.gamepad.pad.buttons[0].pressed;
-}
+  return this._buttons[0].pressed;
+};
 
 prototype.bButtonPressed = function () {
-  return this.gamepad.pad.buttons[1].pressed;
-}
+  return this._buttons[1].pressed;
+};
 
 prototype.xButtonPressed = function () {
-  return this.gamepad.pad.buttons[2].pressed;
-}
+  return this._buttons[2].pressed;
+};
 
 prototype.yButtonPressed = function () {
-  return this.gamepad.pad.buttons[3].pressed;
-}
+  return this._buttons[3].pressed;
+};
 
 prototype.leftBumperPressed = function () {
-  return this.gamepad.pad.buttons[4].pressed;
-}
+  return this._buttons[4].pressed;
+};
 
 prototype.rightBumperPressed = function () {
-  return this.gamepad.pad.buttons[5].pressed;
-}
+  return this._buttons[5].pressed;
+};
 
 prototype.leftTriggerPressed = function () {
-  return this.gamepad.pad.buttons[6].pressed;
-}
+  return this._buttons[6].pressed;
+};
 
 prototype.rightTriggerPressed = function () {
-  return this.gamepad.pad.buttons[7].pressed;
-}
+  return this._buttons[7].pressed;
+};
 
 prototype.backButtonPressed = function () {
-  return this.gamepad.pad.buttons[8].pressed;
-}
+  return this._buttons[8].pressed;
+};
 
 prototype.startButtonPressed = function () {
-  return this.gamepad.pad.buttons[9].pressed;
-}
+  return this._buttons[9].pressed;
+};
 
 prototype.leftAnalogStickPressed = function () {
-  return this.gamepad.pad.buttons[10].pressed;
-}
+  return this._buttons[10].pressed;
+};
 
 prototype.rightAnalogStickPressed = function () {
-  return this.gamepad.pad.buttons[11].pressed;
-}
+  return this._buttons[11].pressed;
+};
 
 prototype.dPadUpPressed = function () {
-  return this.gamepad.pad.buttons[12].pressed;
-}
+  return this._buttons[12].pressed;
+};
 
 prototype.dPadDownPressed = function () {
-  return this.gamepad.pad.buttons[13].pressed;
-}
+  return this._buttons[13].pressed;
+};
 
 prototype.dPadLeftPressed = function () {
-  return this.gamepad.pad.buttons[14].pressed;
-}
+  return this._buttons[14].pressed;
+};
 
 prototype.dPadRightPressed = function () {
-  return this.gamepad.pad.buttons[15].pressed;
-}
+  return this._buttons[15].pressed;
+};
 
 prototype.guideButtonPressed = function () {
   throw new Error('Vendor button is not supported');
-}
+};
 
 prototype.leftStickX = function () {
-  return this.gamepad.pad.axes[0];
-}
+  return this._pad.axes[0];
+};
 
 prototype.leftStickY = function () {
-  return this.gamepad.pad.axes[1];
-}
+  return this._pad.axes[1];
+};
 
 prototype.rightStickX = function () {
-  return this.gamepad.pad.axes[2];
-}
+  return this._pad.axes[2];
+};
 
 prototype.rightStickY = function () {
-  return this.gamepad.pad.axes[3];
-}
+  return this._pad.axes[3];
+};

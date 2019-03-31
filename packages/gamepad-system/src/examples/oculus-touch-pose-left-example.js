7d1a8e592c3d8a724372b6d7b63bb5e48371416d
Lars Wobus <lars.wobus@web.de>
2019-03-31 16:07:19 +0200

    Default setup to track oculus rift controllers

--- packages/gamepad-system/src/examples/oculus-touch-pose-left-example.js
+++ packages/gamepad-system/src/examples/oculus-touch-pose-left-example.js
@@ -1,6 +1,6 @@
 const { attributes, prototype } = pc.createScript('OculusTouchPoseLeftExample');
 
-attributes.add('_oculusTouchInputLeftEntity', {
+attributes.add('_targetEntity', {
   type: 'entity',
   title: 'Oculus Touch Input Left Entity',
   description: 'Entity with a OculusTouchInputLeft script attached',
@@ -11,14 +11,51 @@ attributes.add('_modelEntity', {
   title: 'Model Entity',
   description: 'Entity with a model component',
 });
+    
+attributes.add('_offset', {
+  type: 'vec3',
+  title: 'Offset',
+  default: [0, 3.4, 0],
+  description: ''
+});
 
 prototype.initialize = function () {
-  const { OculusTouchInputLeft } = this._oculusTouchInputLeftEntity.script;
+  const { vr } = this.app;
+  if (!vr || !vr.display) {
+    console.error('VR not supported');
+    this.enabled = false;
+    return;
+  }
+
+  const { OculusTouchInputLeft } = this._targetEntity.script;
   this.enabled = OculusTouchInputLeft.enabled;
-  OculusTouchInputLeft.on("state", (enabled) => { this.enabled = enabled; });
+  OculusTouchInputLeft.on(
+    'state',
+    (enabled) => { this.enabled = enabled; }
+  );
+  this.OculusTouchInputLeft = OculusTouchInputLeft;
 };
 
 prototype.update = function () {
-  const position = this._oculusTouchInputLeftEntity.script.OculusTouchInputLeft.position();
-  this._modelEntity.setPosition(position[0], position[1], position[2]);
+  const { OculusTouchInputLeft } = this;
+  const position = OculusTouchInputLeft.position();
+
+  if (!position) {
+    return;
+  }
+
+  const { _offset } = this;
+  this._modelEntity.setPosition(
+    position[0] + _offset.x,
+    position[1] + _offset.y,
+    position[2] + _offset.z
+  );
+
+  const orientation = OculusTouchInputLeft.orientation();
+  this._modelEntity.setRotation(
+    orientation[0],
+    orientation[1],
+    orientation[2],
+    orientation[3]
+  );
 };
--- packages/gamepad-system/src/examples/oculus-touch-pose-right-example.js
+++ packages/gamepad-system/src/examples/oculus-touch-pose-right-example.js
@@ -1,6 +1,6 @@
 const { attributes, prototype } = pc.createScript('OculusTouchPoseRightExample');
 
-attributes.add('_oculusTouchInputRightEntity', {
+attributes.add('_targetEntity', {
   type: 'entity',
   title: 'Oculus Touch Input Right Entity',
   description: 'Entity with a OculusTouchInputRight script attached',
@@ -12,13 +12,50 @@ attributes.add('_modelEntity', {
   description: 'Entity with a model component',
 });
 
+attributes.add('_offset', {
+  type: 'vec3',
+  title: 'Offset',
+  default: [0, 3.4, 0],
+  description: ''
+});
+
 prototype.initialize = function () {
-  const { OculusTouchInputRight } = this._oculusTouchInputRightEntity.script;
+  const { vr } = this.app;
+  if (!vr || !vr.display) {
+    console.error('VR not supported');
+    this.enabled = false;
+    return;
+  }
+
+  const { OculusTouchInputRight } = this._targetEntity.script;
   this.enabled = OculusTouchInputRight.enabled;
-  OculusTouchInputRight.on("state", (enabled) => { this.enabled = enabled; });
+  OculusTouchInputRight.on(
+    'state',
+    (enabled) => { this.enabled = enabled; }
+  );
+  this.OculusTouchInputRight = OculusTouchInputRight;
 };
 
 prototype.update = function () {
-  const position = this._oculusTouchInputRightEntity.script.OculusTouchInputRight.position();
-  this._modelEntity.setPosition(position[0], position[1], position[2]);
+  const { OculusTouchInputRight } = this;
+  const position = OculusTouchInputRight.position();
+
+  if (!position) {
+    return;
+  }
+
+  const { _offset } = this;
+  this._modelEntity.setPosition(
+    position[0] + _offset.x,
+    position[1] + _offset.y,
+    position[2] + _offset.z
+  );
+
+  const orientation = OculusTouchInputRight.orientation();
+  this._modelEntity.setRotation(
+    orientation[0],
+    orientation[1],
+    orientation[2],
+    orientation[3]
+  );  
 };
